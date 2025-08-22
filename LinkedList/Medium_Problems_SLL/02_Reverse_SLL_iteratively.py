b3c20d48e5d266a2fd9d851e85105bf080392b4d
Rishi Dwivedi <121298183+Quasar-28@users.noreply.github.com>
2025-08-22 18:54:59 +0530

    Delete LinkedList directory

--- LinkedList/Medium_Problems_SLL/02_Reverse_SLL_iteratively.py
+++
@@ -1,9 +0,0 @@
-# see leetcode problem 206
-# Problem Statement: Given the head of a singly linked list, write a program to reverse the linked list, and return the head pointer to the reversed list.
-class Node :
-    def __init__(self,item=None,next=None):
-        self.item = item
-        self.next = next 
-def reverse_SLL(head) :
-
-    
